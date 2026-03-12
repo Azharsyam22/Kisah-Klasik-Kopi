f49a931cc6d908088613162c5d965b7be0f1829b
Azharsyam22 <azharsyam22@gmail.com>
2026-03-13 00:14:13 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Kisah-Klasik-Kopi/', // <-- WAJIB SESUAI NAMA REPO GITHUB KAMU, JANGAN LUPA GARIS MIRING DI AWAL DAN AKHIR
 })
